65255207e209f8635641c36e6dea968464824b96
Aliaksei Pilko <aliakseipilko1@gmail.com>
2017-03-05 09:49:33 +0000

    Fixed datetime actioned not updating

--- src/app/models.py
+++ src/app/models.py
@@ -14,14 +14,14 @@ def make_save_file_path(instance, filename):
 
 
 class PrintJob(models.Model):
-
     print_id = models.UUIDField(default=uuid.uuid4, editable=False, blank=False, primary_key=True, unique=True,
                                 auto_created=True)
     print_name = models.CharField(max_length=20, verbose_name="Print Name", blank=False)
     requester = models.ForeignKey(Profile, on_delete=models.CASCADE, editable=False, auto_created=True)
 
     datetime_submitted = models.DateTimeField(editable=False, auto_now_add=True, verbose_name='Job submitted')
-    datetime_actioned = models.DateTimeField(editable=False, blank=True, null=True, verbose_name="Job last actioned")
+    datetime_actioned = models.DateTimeField(editable=False, blank=True, null=True, auto_now=True,
+                                             verbose_name="Job last actioned")
 
     notes = models.TextField(verbose_name='Notes', blank=True, null=True)
     file = models.FileField(verbose_name="CAD File", blank=False, upload_to=make_save_file_path)
